56eac2eee49b4f0049945e03082bbb7e1f437e62
rrajan10781-netizen <rrajan10781@gmail.com>
2026-07-30 12:05:50 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,16 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a;
+
+        a = 10;
+        System.out.println("a = " + a);
+
+        a += 5;
+        System.out.println("a += 5 : " + a);
+
+        a -= 3;
+        System.out.println("a -= 3 : " + a);
+
+       
+    }
+}
